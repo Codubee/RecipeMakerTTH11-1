85a3a20774aeb3a852cc8cdbee767a008ffb82fe
Sung Hyun Hwang <sh.hwang1012@gmail.com>
2020-11-02 23:14:10 -0600

    Added the Tools.js componenet and verified a test of Tools.js displays properly on landing page.

--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -1,10 +1,13 @@
 import React from 'react'
 //Import components that you create here
+import Tools from '../Tools.js';
 
 function LandingPage(){
     return(
         //Use components that you import here
-        <></>
+        <>
+            <Tools/>
+        </>
     )
 }
 
---
+++ frontend/src/components/Tools.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Tools() {
+    return <h1>Test for Tools.js</h1>;
+}
+
+export default Tools;
