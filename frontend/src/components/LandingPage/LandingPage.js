036992a13a8b3bba992a4d991fcf8a875968ef16
Adem Odza <62858601+AdemOdza@users.noreply.github.com>
2020-11-04 22:39:09 -0600

    Started out with Navbar template, having some issues rendering.

--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -1,10 +1,15 @@
 import React from 'react'
 //Import components that you create here
+import navbar from '../../navbar'
 
 function LandingPage(){
     return(
         //Use components that you import here
-        <></>
+        <>
+             <navbar/>
+        </> 
+       
+        
     )
 }
 
---
+++ frontend/src/navbar.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    NavbarText
+  } from 'reactstrap';
+
+
+function navbar()
+{
+    return( 
+    <div> 
+        <h1>Testing</h1>
+    </div>
+    );
+}
+
+    /*  <Navbar>
+            <NavbarBrand> 
+            Home 
+            </NavbarBrand>
+
+            <NavbarBrand href="https://github.com/Codubee/RecipeMakerTTH11-1">
+            GitHub
+            </NavbarBrand>
+
+            <NavbarBrand>
+            Second
+            </NavbarBrand>
+
+        </Navbar> 
+        
+        TO DO: Find out how to link to the home page, find out why nothing is rendering on the app, find out the correct labels for the navbar
+        */
+export default navbar
