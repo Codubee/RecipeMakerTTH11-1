9af290bbc5c4083ffca061b1dc93e0280ecd0c02
Sung Hyun Hwang <sh.hwang1012@gmail.com>
2020-11-10 10:34:29 -0600

    Fixed some centering of the logo images used.

--- frontend/src/components/LandingPage/Tools/Tools.js
+++ frontend/src/components/LandingPage/Tools/Tools.js
@@ -13,19 +13,19 @@ import {
       <div>
         <Jumbotron fluid>
           <Container fluid>
-            <h1 className="display-3" style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>Built Using</h1>
-            <hr className="my-4" />
+            <h1 className="display-4" style={{ textAlign: "center" }}>Built Using</h1>
+            <hr className="my-4" width="250" />
             <div id="frontend-backend-logos" style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-              <img id="nodejs-logo" src={nodejsLogo} alt="Nodejs logo" height="%100" width="225" style={{ marginLeft: "100px" }}/> 
-              <img id="react-logo" src={reactLogo} alt="React logo" height="100%" width="250" />
-              <img id="heroku-logo" src={herokuLogo} alt="Heroku logo" height="%100" width="350" />
+              <img id="nodejs-logo" src={nodejsLogo} alt="Nodejs logo" height="%100" width="225" style={{ marginLeft: "120px", marginRight: "35px" }}/> 
+              <img id="react-logo" src={reactLogo} alt="React logo" height="100%" width="250"/>
+              <img id="heroku-logo" src={herokuLogo} alt="Heroku logo" height="%100" width="350" style={{ marginLeft: "20px" }}/>
             </div>
             <div id="api-logos" style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-              {/*<img src={} alt="Image analyzer logo" /> */}
-              <img id="youtube-logo" src={youtubeLogo} alt="YouTube logo" height="100%" width="75" />
-              {/*<img src={} alt="Recipe maker logo" /> */}
-              <img id="yelp-logo" src={yelpLogo} alt="Yelp logo" height="100%" width="150" />
-              {/*<img src={} alt="Food nutrition logo" /> */}
+              {/*<img src={} alt="Image analyzer logo"/> */}
+              <img id="youtube-logo" src={youtubeLogo} alt="YouTube logo" height="100%" width="75" style={{ marginLeft: "60px" }}/>
+              {/*<img src={} alt="Recipe maker logo"/> */}
+              <img id="yelp-logo" src={yelpLogo} alt="Yelp logo" height="100%" width="150"/>
+              {/*<img src={} alt="Food nutrition logo"/> */}
             </div>
           </Container>
         </Jumbotron>
