e12c502207b9abca4524de477c349184f7cb8b78
Sung Hyun Hwang <sh.hwang1012@gmail.com>
2020-11-10 00:31:46 -0600

    Centered h1 and img contents. Added a thematic break hr after the jumbotron h1.

--- frontend/src/components/LandingPage/Tools/Tools.js
+++ frontend/src/components/LandingPage/Tools/Tools.js
@@ -13,13 +13,14 @@ import {
       <div>
         <Jumbotron fluid>
           <Container fluid>
-            <h1 className="display-3">Built Using</h1>
-            <div id="frontend-backend-logos">
+            <h1 className="display-3" style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>Built Using</h1>
+            <hr className="my-4" />
+            <div id="frontend-backend-logos" style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+              <img id="nodejs-logo" src={nodejsLogo} alt="Nodejs logo" height="%100" width="225" style={{ marginLeft: "100px" }}/> 
               <img id="react-logo" src={reactLogo} alt="React logo" height="100%" width="250" />
-              <img id="nodejs-logo" src={nodejsLogo} alt="Nodejs logo" height="%100" width="225" /> 
               <img id="heroku-logo" src={herokuLogo} alt="Heroku logo" height="%100" width="350" />
             </div>
-            <div id="api-logos">
+            <div id="api-logos" style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
               {/*<img src={} alt="Image analyzer logo" /> */}
               <img id="youtube-logo" src={youtubeLogo} alt="YouTube logo" height="100%" width="75" />
               {/*<img src={} alt="Recipe maker logo" /> */}
