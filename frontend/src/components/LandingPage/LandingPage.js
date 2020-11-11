9417a058ba4a9c6137d007f3272e6ed973da9dba
Sung Hyun Hwang <sh.hwang1012@gmail.com>
2020-11-10 23:23:01 -0600

    Fixed spacing issues between each logo image by using the Bootstrap grid system. Also added repsonsive features to the logo layout in the css file.

--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -1,5 +1,4 @@
 import React from 'react'
-//Import components that you create here
 import Tools from './Tools/Tools';
 
 function LandingPage(){
--- frontend/src/components/LandingPage/Tools/Tools.js
+++ frontend/src/components/LandingPage/Tools/Tools.js
@@ -6,26 +6,29 @@ import youtubeLogo from './assets/youtube-logo.png';
 import yelpLogo from './assets/yelp-logo.png';
 import './Tools.css';
 import {
-    Jumbotron, Container, Tooltip,
+    Jumbotron, Container
   } from 'reactstrap';
 
-  const Tools = (props) => {
+  function Tools(props) {
     return (
       <div>
         <Jumbotron fluid>
           <Container fluid>
             <h1 id="tools-header" className="display-4">Built Using</h1>
             <hr className="my-4" width="250" />
-            <div id="main-logos">
-              <img id="nodejs-logo" src={nodejsLogo} alt="Nodejs logo" height="%100" width="225"/> 
-              <img id="react-logo" src={reactLogo} alt="React logo" height="100%" width="250"/>
-              <img id="heroku-logo" src={herokuLogo} alt="Heroku logo" height="%100" width="350"/>
+            <div class="row" id="main-logos">
+              <img class="col-sm-2" id="nodejs-logo" src={nodejsLogo} alt="Nodejs logo"/> 
+              <img class="col-sm-2" id="react-logo" src={reactLogo} alt="React logo"/>
+              <img class="col-sm-3" id="heroku-logo" src={herokuLogo} alt="Heroku logo"/>
             </div>
-            <div id="api-logos">
+            <div class="text-center">
+              <p className="lead">with APIs from</p>
+            </div>
+            <div class="row" id="api-logos">
               {/*<img src={} alt="Image analyzer logo"/> */}
-              <img id="youtube-logo" src={youtubeLogo} alt="YouTube logo" height="100%" width="75"/>
+              <img class="col-sm-1" src={youtubeLogo} alt="YouTube logo"/>
               {/*<img src={} alt="Recipe maker logo"/> */}
-              <img id="yelp-logo" src={yelpLogo} alt="Yelp logo" height="100%" width="150"/>
+              <img class="col-sm-1" src={yelpLogo} alt="Yelp logo"/>
               {/*<img src={} alt="Food nutrition logo"/> */}
             </div>
           </Container>
