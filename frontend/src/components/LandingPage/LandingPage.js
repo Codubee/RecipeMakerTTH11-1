90951442c378412e05613363e8701208011bd214
Stacey-Masi <staceymasi@gmail.com>
2020-11-15 10:05:15 -0600

    attemmpting to fix merge conflicts

--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -1,10 +1,8 @@
 import React from 'react'
-//Import components that you create here
 import Features from './Features/Features.js'
 
 function LandingPage(){
     return(
-        //Use components that you import here
         <>
             <Features/>
         </>
