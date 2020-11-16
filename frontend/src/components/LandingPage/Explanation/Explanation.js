f228f851e194ed35a98072ee02deb24a0e20bf11
alekhyapinnamaneni <alekhya.pinnamaneni.tx@gmail.com>
2020-11-16 11:35:44 -0600

    Changed color theme.

--- frontend/src/components/LandingPage/Explanation/Explanation.js
+++ frontend/src/components/LandingPage/Explanation/Explanation.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Jumbotron } from 'reactstrap';
 
-const Explanation = (props) =>
+document.body.style.backgroundColor = "#e3c3b2";
+
+function Explanation()
 {
     return (
         <div>
-            <Jumbotron>
-                <h1 className="display-3">Recipe Maker</h1>
-                <p className="lead">Simply upload pictures or type in the ingredients you have in your pantry and fridge, and Recipe Maker will generate a matching recipe.</p>
+            <Jumbotron style={{backgroundColor: '#284d46'}}>
+                <h1 className="display-3" style={{color: '#e3c3b2'}}>Recipe Maker</h1>
+                <p className="lead" style={{color: '#e3c3b2'}}>Simply upload pictures or type in the ingredients you have in your pantry and fridge, and Recipe Maker will generate a matching recipe.</p>
             </Jumbotron>
         </div>
     );
