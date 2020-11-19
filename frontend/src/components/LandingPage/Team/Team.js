7f8e6cecdd2737ed09e06381d789c3fade2702ce
Sai Abhiroop Tanjavuru <saiabhirooptanjavuru@Sais-MBP.attlocal.net>
2020-11-19 01:12:45 -0600

    Added theme, rearranged cards, and added two buttons

--- frontend/src/components/LandingPage/Team/Team.js
+++ frontend/src/components/LandingPage/Team/Team.js
@@ -3,31 +3,65 @@ This component will have a title and description of the team members.
 */
 
 import React from 'react';
-import { Card, CardTitle, CardText } from 'reactstrap';
+import { Card, Button, CardTitle, CardText, CardSubtitle, CardDeck} from 'reactstrap';
+
+document.body.style.backgroundColor="#e3c3b2";
 
 function Team(){
     return (
-        <div>
-            <Card body inverse color="primary">
-                <CardTitle tag="h5">Alekhya Pinnamaneni</CardTitle>
-                <CardText>Worked on explanation component</CardText>
-            </Card>
-            <Card body inverse color="info">
-                <CardTitle tag="h5">Sung Hyun Hwang</CardTitle>
-                <CardText>Worked on tool component</CardText>
-            </Card>
-            <Card body inverse color="primary">
-                <CardTitle tag="h5">Stacey Masi</CardTitle>
-                <CardText>Worked on features component</CardText>
-            </Card>
-            <Card body inverse color="info">
-                <CardTitle tag="h5">Sai Abhiroop Tanjavuru</CardTitle>
-                <CardText>Worked on team component</CardText>
-            </Card>
-            <Card body inverse color="primary">
-                <CardTitle tag="h5">Adem Odza</CardTitle>
-                <CardText>Worked on navbar</CardText>
-            </Card>
+        <div className ="container">
+            <CardDeck>
+                    <Card body className = "text-center" inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>
+                        <CardTitle tag="h4">Alekhya Pinnamaneni</CardTitle>
+                        <br></br>
+                        <CardSubtitle tag ="h5">Explanation component</CardSubtitle>
+                        <br></br>
+                        <CardText tag ="h6">Info about ourselves</CardText>
+                        <br></br>
+                        <Button inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>GitHub</Button>
+                        <Button inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>LinkedIn</Button>
+                    </Card>
+                    <Card body className = "text-center" inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>
+                        <CardTitle tag="h4">Sung Hyun Hwang</CardTitle>
+                        <br></br>
+                        <CardSubtitle tag ="h5">Tool component</CardSubtitle>
+                        <br></br>
+                        <CardText tag="h6">Info about ourselves</CardText>
+                        <br></br>
+                        <Button inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>GitHub</Button>
+                        <Button inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>LinkedIn</Button>
+                    </Card>
+                    <Card body className = "text-center" inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>
+                        <CardTitle tag="h4">Stacey Masi</CardTitle>
+                        <br></br>
+                        <CardSubtitle tag="h5">Features component</CardSubtitle>
+                        <br></br>
+                        <CardText tag="h6">Info about ourselves</CardText>
+                        <br></br>
+                        <Button inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>GitHub</Button>
+                        <Button inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>LinkedIn</Button>
+                    </Card>
+                    <Card body className = "text-center" inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>
+                        <CardTitle tag="h4">Sai Abhiroop Tanjavuru</CardTitle>
+                        <br></br>
+                        <CardSubtitle tag="h5">Team component</CardSubtitle>
+                        <br></br>
+                        <CardText tag="h6">Info about ourselves</CardText>
+                        <br></br>
+                        <Button inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>GitHub</Button>
+                        <Button inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>LinkedIn</Button>
+                    </Card>
+                    <Card body className = "text-center" inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>
+                        <CardTitle tag="h4">Adem Odza</CardTitle>
+                        <br></br>
+                        <CardSubtitle tag="h5">Navbar</CardSubtitle>
+                        <br></br>
+                        <CardText tag="h6">Info about ourselves</CardText>
+                        <br></br>
+                        <Button inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>GitHub</Button>
+                        <Button inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>LinkedIn</Button>
+                    </Card>
+            </CardDeck>
         </div>
     );
 };
