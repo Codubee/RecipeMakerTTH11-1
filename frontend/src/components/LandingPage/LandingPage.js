73358c6bdd01fc0c56ede5374c76360aba036126
alekhyapinnamaneni <72823695+alekhyapinnamaneni@users.noreply.github.com>
2020-11-22 21:15:45 -0600

    CB-1- Explanation (#1) Added explanation component
    * Explanation
    
    * import Explanation
    
    * Changed color theme.
    
    * Moved inline styles to stylesheet
    
    Co-authored-by: Miguel <info@codubee.com>

--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Explanation from './Explanation/Explanation';
 import Tools from './Tools/Tools';
 import Features from './Features/Features.js'
 
 function LandingPage(){
     return(
         <>
+            <Explanation/>
             <Tools/>
             <Features/>
         </>
