85a1c54e3ad53e458fe645ab312f5e4ccb7e8d11
Sai Abhiroop Tanjavuru <saiabhirooptanjavuru@Sais-MBP.attlocal.net>
2020-11-22 21:39:37 -0600

    Inline styling for cards removed and moved to CSS. Github buttons now link to our individual Github. h1 className changed to home display-4

--- frontend/src/components/LandingPage/Team/Team.js
+++ frontend/src/components/LandingPage/Team/Team.js
@@ -11,62 +11,63 @@ document.body.style.backgroundColor="#e3c3b2";
 function Team(){
     return (
         <div className ="container">
-        <h1 className = "home">Meet the Team!</h1>
+        <h1 className = "home display-4">Meet the Team!</h1>
 
             <br></br>
-            <CardDeck>
-                    <Card body className = "text-center" inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>
+            <CardDeck className="text-white">
+                    
+                    <Card body className = "text-center cardStyling">
                         <CardTitle tag="h4">Alekhya Pinnamaneni</CardTitle>
                         <br></br>
                         <CardSubtitle tag ="h5">Major and Class</CardSubtitle>
                         <br></br>
                         <CardText tag ="h6">(Info about ourselves)</CardText>
                         <br></br>
-                        <Button inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>GitHub</Button>
+                        <Button href="https://github.com/alekhyapinnamaneni" target="_blank" inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>GitHub</Button>
                         <Button inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>LinkedIn</Button>
                     </Card>
 
-                    <Card body className = "text-center" inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>
+                    <Card body className = "text-center cardStyling">
                         <CardTitle tag="h4">Sung Hyun Hwang</CardTitle>
                         <br></br>
                         <CardSubtitle tag ="h5">Major and Class</CardSubtitle>
                         <br></br>
                         <CardText tag="h6">(Info about ourselves)</CardText>
                         <br></br>
-                        <Button inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>GitHub</Button>
+                        <Button href="https://github.com/sunghyun96" target="_blank" inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>GitHub</Button>
                         <Button inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>LinkedIn</Button>
                     </Card>
 
-                    <Card body className = "text-center" inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>
+                    <Card body className = "text-center cardStyling">
                         <CardTitle tag="h4">Stacey Masi</CardTitle>
                         <br></br>
                         <CardSubtitle tag="h5">Major and Class</CardSubtitle>
                         <br></br>
                         <CardText tag="h6">(Info about ourselves)</CardText>
                         <br></br>
-                        <Button inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>GitHub</Button>
+                        <Button href="https://github.com/Stacey-Masi" target="_blank" inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>GitHub</Button>
                         <Button inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>LinkedIn</Button>
                     </Card>
 
-                    <Card body className = "text-center" inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>
+                    <Card body className = "text-center cardStyling">
                         <CardTitle tag="h4">Sai Abhiroop Tanjavuru</CardTitle>
                         <br></br>
                         <CardSubtitle tag="h5">Major and Class</CardSubtitle>
                         <br></br>
                         <CardText tag="h6">(Info about ourselves)</CardText>
                         <br></br>
-                        <Button inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>GitHub</Button>
+                        <Button href="https://github.com/saitanjavuru" target="_blank" inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>GitHub</Button>
                         <Button inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>LinkedIn</Button>
                     </Card>
 
-                    <Card body className = "text-center" inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>
+                    <Card body className = "text-center cardStyling">
                         <CardTitle tag="h4">Adem Odza</CardTitle>
                         <br></br>
                         <CardSubtitle tag="h5">Major and Class</CardSubtitle>
                         <br></br>
                         <CardText tag="h6">(Info about ourselves)</CardText>
                         <br></br>
-                        <Button inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>GitHub</Button>
+                        <Button href="https://github.com/AdemOdza" target="_blank" inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>GitHub</Button>
                         <Button inverse style={{backgroundColor: '#2D4D46', borderColor: '#F1EEE9'}}>LinkedIn</Button>
                     </Card>
             </CardDeck>
