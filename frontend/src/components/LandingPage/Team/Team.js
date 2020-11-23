a5481b46cccbf6797ea1dafacac59d20847776d5
Sai Abhiroop Tanjavuru <saiabhirooptanjavuru@Sais-MBP.attlocal.net>
2020-11-23 00:47:17 -0600

    Restructured Team.js and created TeamCard.js so there is no repetition of Cards. Passing in info as props

--- frontend/src/components/LandingPage/Team/Team.js
+++ frontend/src/components/LandingPage/Team/Team.js
@@ -3,80 +3,61 @@ This component will have a title and description of the team members.
 */
 
 import React from 'react';
-import { Card, Button, CardTitle, CardText, CardSubtitle, CardDeck} from 'reactstrap';
+import {CardDeck} from 'reactstrap';
+import TeamCard from './TeamCard';
 import './Team.css'
 
 function Team(){
-    return (
+    return(
         <div className ="container componentBGColor">
         <h1 className = "home display-4">Meet the Team!</h1>
 
             <br></br>
             <CardDeck className="text-white">
-                    
-                    <Card body className = "text-center cardStyling">
-                        <CardTitle tag="h4" className ="titleText">Alekhya Pinnamaneni</CardTitle>
-                        <br></br>
-                        <CardSubtitle tag ="h5">Major and Class</CardSubtitle>
-                        <br></br>
-                        <CardText tag ="h6">(Info about ourselves)</CardText>
-                        <br></br>
-                        <Button className="buttonStyling" href="https://github.com/alekhyapinnamaneni" target="_blank" >GitHub</Button>
-                        <br></br>
-                        <Button className="buttonStyling">LinkedIn</Button>
-                    </Card>
+                <TeamCard
+                    fName = "Alekhya"
+                    lName = "Pinnamaneni"
+                    subtitle = "Major and Class"
+                    text = "(Info about ourselves)"
+                    github = "https://github.com/alekhyapinnamaneni"
+                />
 
-                    <Card body className = "text-center cardStyling">
-                        <CardTitle tag="h4" className ="titleText">Sung Hyun Hwang</CardTitle>
-                        <br></br>
-                        <CardSubtitle tag ="h5">Major and Class</CardSubtitle>
-                        <br></br>
-                        <CardText tag="h6">(Info about ourselves)</CardText>
-                        <br></br>
-                        <Button className="buttonStyling" href="https://github.com/sunghyun96" target="_blank">GitHub</Button>
-                        <br></br>
-                        <Button className="buttonStyling">LinkedIn</Button>
-                    </Card>
+                <TeamCard
+                    fName = "Sung Hyun"
+                    lName = "Hwang"
+                    subtitle = "Major and Class"
+                    text = "(Info about ourselves)"
+                    github = "https://github.com/sunghyun96"
+                />
 
-                    <Card body className = "text-center cardStyling">
-                        <CardTitle tag="h4" className ="titleText">Stacey<br></br>Masi</CardTitle>
-                        <br></br>
-                        <CardSubtitle tag="h5">Major and Class</CardSubtitle>
-                        <br></br>
-                        <CardText tag="h6">(Info about ourselves)</CardText>
-                        <br></br>
-                        <Button className="buttonStyling" href="https://github.com/Stacey-Masi" target="_blank">GitHub</Button>
-                        <br></br>
-                        <Button className="buttonStyling">LinkedIn</Button>
-                    </Card>
+                <TeamCard
+                    fName = "Stacey"
+                    lName = "Masi"
+                    subtitle = "Major and Class"
+                    text = "(Info about ourselves)"
+                    github = "https://github.com/Stacey-Masi"
+                />
 
-                    <Card body className = "text-center cardStyling">
-                        <CardTitle tag="h4" className ="titleText">Sai Abhiroop Tanjavuru</CardTitle>
-                        <br></br>
-                        <CardSubtitle tag="h5">Major and Class</CardSubtitle>
-                        <br></br>
-                        <CardText tag="h6">(Info about ourselves)</CardText>
-                        <br></br>
-                        <Button className="buttonStyling" href="https://github.com/saitanjavuru" target="_blank">GitHub</Button>
-                        <br></br>
-                        <Button className="buttonStyling">LinkedIn</Button>
-                    </Card>
+                <TeamCard
+                    fName = "Sai Abhiroop"
+                    lName = "Tanjavuru"
+                    subtitle = "Major and Class"
+                    text = "(Info about ourselves)"
+                    github = "https://github.com/saitanjavuru"
+                />
 
-                    <Card body className = "text-center cardStyling">
-                        <CardTitle tag="h4" className ="titleText">
-                            Adem<br></br>Odza</CardTitle>
-                        <br></br>
-                        <CardSubtitle tag="h5">Major and Class</CardSubtitle>
-                        <br></br>
-                        <CardText tag="h6">(Info about ourselves)</CardText>
-                        <br></br>
-                        <Button className="buttonStyling" href="https://github.com/AdemOdza" target="_blank">GitHub</Button>
-                        <br></br>
-                        <Button className="buttonStyling">LinkedIn</Button>
-                    </Card>
+                <TeamCard
+                    fName = "Adem"
+                    lName = "Odza"
+                    subtitle = "Major and Class"
+                    text = "(Info about ourselves)"
+                    github = "https://github.com/AdemOdza"
+                />
             </CardDeck>
+
         </div>
-    );
-};
+    )
+
+}
 
 export default Team;
---
+++ frontend/src/components/LandingPage/Team/TeamCard.js
@@ -0,0 +1,26 @@
+/*
+TeamCard.js for repeated Team Cards
+*/
+
+import React from 'react';
+import { Card, Button, CardTitle, CardText, CardSubtitle, CardDeck} from 'reactstrap';
+import './Team.css'
+
+function TeamCard(props){
+    return(
+        <Card body className = "text-center cardStyling">
+            <CardTitle tag="h4" className ="titleText">{props.fName}<br></br>{props.lName}</CardTitle>
+            <br></br>
+            <CardSubtitle tag ="h5">{props.subtitle}</CardSubtitle>
+            <br></br>
+            <CardText tag ="h6">{props.text}</CardText>
+            <br></br>
+            <Button className="buttonStyling" href={props.github} target="_blank" >GitHub</Button>
+            <br></br>
+            <Button className="buttonStyling">LinkedIn</Button>
+        </Card>
+    )
+
+}
+
+export default TeamCard;
