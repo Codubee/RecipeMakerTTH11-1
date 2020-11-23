fae25ed17725bba4d40d614ed13cc5f6dadc04c9
Miguel <info@codubee.com>
2020-11-22 21:13:42 -0600

    Moved inline styles to stylesheet

--- frontend/src/components/LandingPage/Explanation/Explanation.js
+++ frontend/src/components/LandingPage/Explanation/Explanation.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Jumbotron } from 'reactstrap';
+import './Explanation.css'
 
-document.body.style.backgroundColor = "#e3c3b2";
+// document.body.style.backgroundColor = "#e3c3b2";
 
 function Explanation()
 {
     return (
-        <div>
-            <Jumbotron style={{backgroundColor: '#284d46'}}>
-                <h1 className="display-3" style={{color: '#e3c3b2'}}>Recipe Maker</h1>
-                <p className="lead" style={{color: '#e3c3b2'}}>Simply upload pictures or type in the ingredients you have in your pantry and fridge, and Recipe Maker will generate a matching recipe.</p>
+        <div >
+            <Jumbotron className="jumbotron-style">
+                <h1 className="display-3" >Recipe Maker</h1>
+                <p className="lead" >Simply upload pictures or type in the ingredients you have in your pantry and fridge, and Recipe Maker will generate a matching recipe.</p>
             </Jumbotron>
         </div>
     );
