4f3391c115dc0ffe43dd225d2cbba5c9c9446639
Adem Odza <62858601+AdemOdza@users.noreply.github.com>
2020-12-08 12:30:10 -0600

    First Commit

--- backend/index.js
+++ backend/index.js
@@ -14,4 +14,25 @@ app.listen(port,()=>{
     console.log('API is up and running')
 })
 
+// Recipe Maker API
+app.get( '/recipe', (req, res)=>{
+
+    //Add key
+    
+    axios.get('https://api.edamam.com/search?q=chicken')
+    .then( (response)=>{
+        console.log(response.data);
+        res.status(200);
+
+    })
+
+    .catch( (error)=>{
+        console.log(error);
+        res.status(400).json({error:'An error has occurred'});
+
+
+    })
+
+})
+
 module.exports = app;
