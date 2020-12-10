857ec076126e89a01ecb41d6b6b5bfe4801237b4
alekhyapinnamaneni <alekhya.pinnamaneni.tx@gmail.com>
2020-12-10 11:22:53 -0600

    Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,44 @@
 import './App.css';
 import LandingPage from './components/LandingPage/LandingPage'
 import MyNavbar from './components/MyNavbar/MyNavbar';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
 function App() {
   return (
     <>
       <MyNavbar/>
-      <LandingPage/>
+      <Router>
+          <Switch>
+            <Route path="/" component={LandingPage} />
+            <Route path="/test" component={Recipes} />
+            <Route path="https://github.com/Codubee/RecipeMakerTTH11-1">
+              <GitHub />
+            </Route>
+          </Switch>
+      </Router>
     </>
   );
+}
+
+function Home()
+{
+  return <h2>Home</h2>
+}
+
+function Recipes()
+{
+  return <h2>Find a recipe</h2>
+}
+
+function GitHub()
+{
+  return <h2>GitHub</h2>
 }
 
 export default App;
