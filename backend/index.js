bdf85ec07b0b3598c1e19cfe5de28459d7ba37d6
Sung Hyun Hwang <sh.hwang1012@gmail.com>
2020-12-14 17:52:29 -0600

    Added post request to send JS object body with recipe name to the API.

--- backend/index.js
+++ backend/index.js
@@ -14,4 +14,25 @@ app.listen(port,()=>{
     console.log('API is up and running')
 })
 
+/**
+ *  Send a post request to the API that will get YouTube links for recipe videos.
+ */
+app.post('/youtubeRecipe',(req,res)=>{
+    console.log(req.body);
+
+    const body = {
+        recipe: req.body.recipe
+    }
+
+    axios.post('https://codubee-api.herokuapp.com/youtubeLinks',body)
+    .then(function(response) { 
+        console.log(response.data);
+        res.status(200).json(response.data);
+    })
+    .catch(function(error) {
+        console.log(error);
+        res.status(400).json({error:"An error has occured"});
+    })
+})
+
 module.exports = app;
