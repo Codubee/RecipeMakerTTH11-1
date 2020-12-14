5256a6d127799f835d40c4a8d3b6ae3c36d7bb3f
Adem Odza <62858601+AdemOdza@users.noreply.github.com>
2020-12-14 14:17:08 -0600

    Added some comments

--- backend/index.js
+++ backend/index.js
@@ -16,27 +16,29 @@ app.listen(port,()=>{
 })
 
 
-// Call Recipe Maker API
+// Call the Recipe Maker API
+// :userIngredients is the string of ingredients to search the API for
+// Example: localhost:8080/recipe/chicken rice beans/ searches for recipes containing chicken, rice, and beans
 app.get( '/recipe/:userIngredients/', (req, res)=>{
 
 
     //API Verification
-    const API_Key = process.env.RECIPEMAKER_API_KEY;
-    const App_ID = process.env.RECIPEMAKER_APP_ID;
+    const API_KEY = process.env.RECIPEMAKER_API_KEY;
+    const APP_ID = process.env.RECIPEMAKER_APP_ID;
     
-    
-    axios.get('https://api.edamam.com/search?q=' + req.params.userIngredients + '&app_key=' + API_Key + '&app_id=' + App_ID)
+    //API Call
+    axios.get('https://api.edamam.com/search?q=' + req.params.userIngredients + '&app_key=' + API_KEY + '&app_id=' + APP_ID)
     .then( (response)=>{
-        //Return only the recipes
+        //Print recipes to console
         console.log(response.data.hits);
         res.status(200);
 
     })
 
+    //Error handling
     .catch( (error)=>{
         console.log(error);
         res.status(400).json({error:'An error has occurred'});
-
 
     })
 
