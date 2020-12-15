58318390a64e4c24bbeb452006ba5fbac93470e0
Stacey-Masi <staceymasi@gmail.com>
2020-12-15 11:55:51 -0600

    changed location parameter

--- backend/index.js
+++ backend/index.js
@@ -14,13 +14,15 @@ app.listen(port,()=>{
     console.log('API is up and running');
 })
 
-// call yelp api
-app.get('/business/:recipe', (req, res)=> {
+// call yelp api such as localhost:8080/business/cheesecake/75227
+app.get('/business/:recipe/:zipcode', (req, res)=> {
+    // headers must have this format to call api
     const headers = { headers: {'Authorization': 'Bearer '+process.env.YELP_API_KEY}}
     let recipe = req.params.recipe;
+    let zipcode = req.params.zipcode;
 
-
-    axios.get('https://api.yelp.com/v3/businesses/search?location=dallas&categories=restaurants&term='+recipe, headers)
+    // '&categories=restaurants' was added to reduce the non-food places that would appear
+    axios.get('https://api.yelp.com/v3/businesses/search?term=' + recipe + '&location=' + zipcode + '&categories=restaurants', headers)
     .then((response)=>{
         console.log(response);
         res.status(200).json(response.data);
