4e71db04d1a85eb964e5e63bcf2ea2b3e8174b09
Stacey-Masi <staceymasi@gmail.com>
2020-12-15 11:27:26 -0600

    added yelp api

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,29 @@ app.use(express.json())
 const port = 8080;
 
 app.get('/',(req,res)=>{
-    res.send('Hello world');
+    res.send('Hello World');
 })
 
 app.listen(port,()=>{
-    console.log('API is up and running')
+    console.log('API is up and running');
 })
+
+// call yelp api
+app.get('/business/:recipe', (req, res)=> {
+    const headers = { headers: {'Authorization': 'Bearer '+process.env.YELP_API_KEY}}
+    let recipe = req.params.recipe;
+
+
+    axios.get('https://api.yelp.com/v3/businesses/search?location=dallas&categories=restaurants&term='+recipe, headers)
+    .then((response)=>{
+        console.log(response);
+        res.status(200).json(response.data);
+    })
+    .catch((error)=>{
+        console.log(error);
+        res.status(400).json({error:'An error has occurred'});
+    })
+})
+
 
 module.exports = app;
