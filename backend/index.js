d96de202f1fc65da576371c4fd121706b3d79588
Sai Abhiroop Tanjavuru <saiabhirooptanjavuru@Sais-MBP.attlocal.net>
2020-12-17 10:51:53 -0600

    Added body containing title, ingr, and yield to send with the POST request

--- backend/index.js
+++ backend/index.js
@@ -17,16 +17,30 @@ app.listen(port,()=>{
 //Calling the Nutritional Analysis API
 
 app.post('/nutritionalinfo',(req,res)=>{
-    
+    console.log(req.body)
+
+    const body = {
+        title: req.body.title,
+        ingr: req.body.ingr,
+        yield: req.body.yield
+
+    }
+
+    // API ID AND KEY
     const NA_ID = process.env.NUTRITIONAL_APP_ID
     const NA_KEY = process.env.NUTRITIONAL_APP_KEY
 
-    axios.post('https://api.edamam.com/api/nutrition-details?app_id=' + NA_ID + '&app_key=' + NA_KEY)
+    axios.post('https://api.edamam.com/api/nutrition-details?app_id=' + NA_ID + '&app_key=' + NA_KEY, body)
     .then((response)=>{
+        /* handle success and sends back recipe object containing number of servings (yield), 
+         *total calories for the recipe (calories), nutrient content by nutrient type (totalNutrients, totalDaily), 
+         *diet and health classification (dietLabels, healthLabels)
+         */
         console.log(response.data);
         res.status(200).json(response.data)
     })
     .catch((error)=>{
+        //error message recieved from API call
         console.log(error);
         res.status(400).json({error:"An error occurred"});
     })
