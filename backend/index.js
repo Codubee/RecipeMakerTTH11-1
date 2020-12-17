c80af58baeb5740681cdb35cf90b8d349494a3da
Sai Abhiroop Tanjavuru <saiabhirooptanjavuru@Sais-MBP.attlocal.net>
2020-12-16 23:27:32 -0600

    First commit to backend Nutrition Info branch. Code is still incomplete

--- backend/index.js
+++ backend/index.js
@@ -14,4 +14,24 @@ app.listen(port,()=>{
     console.log('API is up and running')
 })
 
+//Calling the Nutritional Analysis API
+
+app.post('/nutritionalinfo',(req,res)=>{
+    
+    const NA_ID = process.env.NUTRITIONAL_APP_ID
+    const NA_KEY = process.env.NUTRITIONAL_APP_KEY
+
+    axios.post('https://api.edamam.com/api/nutrition-details?app_id=' + NA_ID + '&app_key=' + NA_KEY)
+    .then((response)=>{
+        console.log(response.data);
+        res.status(200).json(response.data)
+    })
+    .catch((error)=>{
+        console.log(error);
+        res.status(400).json({error:"An error occurred"});
+    })
+
+})
+
+
 module.exports = app;
