8f360a2f00bda090af4c15929a92b26c6aacf633
Stacey-Masi <60723923+Stacey-Masi@users.noreply.github.com>
2020-12-19 13:35:56 -0600

    CB-13-Yelp-API (#9)
    * added yelp api
    
    * changed location parameter
    
    * changed categories from restaurants to food

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,30 @@ app.use(express.json())
 const port = 8080;
 
 app.get('/',(req,res)=>{
-    res.send('Hello world');
+    res.send('Hello World');
 })
 
 app.listen(port,()=>{
-    console.log('API is up and running')
+    console.log('API is up and running');
+})
+
+// call yelp api such as localhost:8080/business/cheesecake/75227
+app.get('/business/:recipe/:zipcode', (req, res)=> {
+    // headers must have this format to call api
+    const headers = { headers: {'Authorization': 'Bearer '+process.env.YELP_API_KEY}}
+    let recipe = req.params.recipe;
+    let zipcode = req.params.zipcode;
+
+    // '&categories=food' was added to reduce the non-food places that would appear
+    axios.get('https://api.yelp.com/v3/businesses/search?term=' + recipe + '&location=' + zipcode + '&categories=food', headers)
+    .then((response)=>{
+        console.log(response);
+        res.status(200).json(response.data);
+    })
+    .catch((error)=>{
+        console.log(error);
+        res.status(400).json({error:'An error has occurred'});
+    })
 })
 
 /**
