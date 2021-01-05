68f9ba4cf95daa00b94af706ba9b86c6d3af1a71
Adem Odza <62858601+AdemOdza@users.noreply.github.com>
2021-01-04 23:11:44 -0600

    Finished functionality of component.

--- frontend/src/components/RecipePage/UserInput/UserInput.js
+++ frontend/src/components/RecipePage/UserInput/UserInput.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react'
 import { Card, CardSubtitle, CardTitle, Form, Button, Input, Col } from 'reactstrap';
+import './UserInput.css'
 
 class UserInput extends React.Component{
     constructor(props){
         super(props);
         this.state = { ingredients: '' }
-        //this.handleClick = this.handleClick.bind(this);
-        //this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.handleChange = this.handleChange.bind(this);
 
 
+    }
+
+    handleClick(){
+        console.log(this.state.ingredients);
+    }
+
+    handleChange(event){
+        this.setState({ingredients: event.target.value});
     }
 
     render()
     {
         return(
         <div>
-            <Col sm="3">
-                <Card body>
-                <CardTitle>Enter your ingredients</CardTitle>
-                <CardSubtitle>Enter your ingredients separated by spaces</CardSubtitle>
-                <Input>Test</Input>
-                <Button>Search</Button>
+            <Col sm="4">
+                <Card body className="card-style">
+                <CardTitle className="title-text">Enter your ingredients</CardTitle>
+                <CardSubtitle className="subtitle-text">Enter your ingredients separated by spaces</CardSubtitle>
+                <Input onChange={this.handleChange}>Test</Input>
+                <Button onClick={this.handleClick}>Search</Button>
                 </Card>
             </Col>
         </div>)
