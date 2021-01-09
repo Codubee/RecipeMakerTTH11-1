d0a98abab93585d3a07aefd97fec07f55a5fd006
Adem Odza <62858601+AdemOdza@users.noreply.github.com>
2021-01-08 19:49:34 -0600

    Changed the input so that the user adds one ingredient at a time. Finished the CSS styling.

--- frontend/src/components/RecipePage/UserInput/UserInput.js
+++ frontend/src/components/RecipePage/UserInput/UserInput.js
@@ -1,37 +1,81 @@
 import React, { Component } from 'react'
-import { Card, CardSubtitle, CardTitle, Form, Button, Input, Col } from 'reactstrap';
+import { Card, CardTitle, Button, Input, Col, Row } from 'reactstrap';
 import './UserInput.css'
+
+
+
 
 class UserInput extends React.Component{
     constructor(props){
         super(props);
-        this.state = { ingredients: '' }
-        this.handleClick = this.handleClick.bind(this);
+        this.state = { ingredients: '', text_data:''}
+
+        this.handleAddClick = this.handleAddClick.bind(this);
+        this.handleSearchClick = this.handleSearchClick.bind(this);
         this.handleChange = this.handleChange.bind(this);
 
 
     }
+    
+    handleAddClick(){
+        if(this.state.text_data !== ''){
 
-    handleClick(){
-        console.log(this.state.ingredients);
-        //Edit to call the api later!
+            var tempIngredient = '';
+
+            if(this.state.ingredients === ''){
+                tempIngredient = this.state.text_data;
+            }
+            else{
+                tempIngredient = this.state.ingredients + ' ' + this.state.text_data;
+            }
+
+            
+            this.setState({ingredients: tempIngredient, text_data: ''});
+            console.log(this.state.ingredients);
+        }
+    }
+
+    handleSearchClick(){
+        //Call API here.
     }
 
     handleChange(event){
-        this.setState({ingredients: event.target.value});
+
+        
+        this.setState({text_data: event.target.value});
+        console.log(this.state);
+        
+        
     }
 
     render()
     {
         return(
         <div>
-            <Col sm="3">
+            <Col sm="4">
                 <Card body className="card-style">
-                <CardTitle className="title-text">Enter your ingredients</CardTitle>
-                <CardSubtitle className="subtitle-text">Type your ingredients and separate them using spaces</CardSubtitle>
-                <Input onChange={this.handleChange}>Test</Input>
-                <h1></h1>
-                <Button onClick={this.handleClick}>Search</Button>
+                    <CardTitle className="title-text">Add your ingredients</CardTitle>
+
+                    <Row>
+
+                        <Col>
+                            <Input onChange={this.handleChange} value={this.state.text_data}>Add your ingredients</Input>
+                        </Col>
+
+                        <Col>
+                            <Button className="button-background" onClick={this.handleAddClick}><text className="button-text">Add</text></Button>
+                        </Col>
+
+                    </Row>
+                    <Row>
+                        <Col>
+                        <text className="subtitle-text">    Your ingredients: {this.state.ingredients}</text>
+                        </Col>
+                    </Row>
+
+
+                    <h1></h1>
+                    <Button className="button-background" onClick={this.handleSearchClick}><text className="button-text">Search</text></Button>
                 </Card>
             </Col>
         </div>)
